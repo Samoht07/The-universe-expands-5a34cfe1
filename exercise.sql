6d33ab4f926beb4e2f60c6daba5737334f12f700
Samoht07 <341737@talnet.nl>
2020-01-31 11:34:39 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,9 @@
+USE sterrenstell;
+-- CREATE TABLE Planeten (name VARCHAR(20));
+INSERT INTO planeten (name) VALUES('Mercurius');
+INSERT INTO planeten (name) VALUES('Aarde');
+INSERT INTO planeten (name) VALUES('Mars');
+INSERT INTO planeten (name) VALUES('Venus');
+INSERT INTO planeten (name) VALUES('Zon');
+SHOW TABLES;
+-- describe planeten;
